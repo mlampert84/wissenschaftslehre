61bfbdb5fbb5ad665a6ed24c4a92e0a64cbd9a4a
Marcus Lampert <marcus.lampert@gmail.com>
2019-08-11 03:16:59 +0200

    Continue with first section

--- frontend/src/commentary/ErsterTeil.elm
+++ frontend/src/commentary/ErsterTeil.elm
@@ -2,12 +2,59 @@ module Commentary.ErsterTeil exposing (content)
 
 
 content =
-    """In this section, Fichte presents the three basic principles that will together form the scaffolding of his entire system.  The three principles are as follows:
-+ 'The I sets originally absolutely its own being.'
-+ 'A Not-I is absolutely set against the I.
-+ Both the I and the Not-I are absolutely posited partially.'
-As Fichte explains in Section 3, these three claims together contain implicitly everything in his system.  He express the unity of these three claims in the following formulation: 'In the I, I set against the divisible I, a divisible Not-I.'  The first part of the formulation expresses the claim from the first principle that all positing occurs in and by the I.  That the I sets a Not-I over and against the I is covered by the second principle. That fact that the I and the Not-I are positied as divisible is then covered by the third principle.
+    """# The three basic principles
+
+In this section, Fichte presents the three basic principles from which the rest of his philosophical system will follow.  Let's take a look at these principles to understand how they work and what they mean.
+
+## The first principle
+The first principle is the most important.  It is the starting and ending point of Fichte's system.  Whereas the second and third principles do not feature prominently in future iterations that Fichte made of the _Wissenschaftslehre_, the first principle always remains the centerpiece of Fichte's philosophy.  He formulates it as follows:
+~~~
+The self sets originally absolutely its own being.
+
+Das sich setzt ursprünglich schlechthin sein eigenes Sein. 
+~~~      
+
+We will discuss what this means below. For now, we can paraphrase this rather cryptic formulation as being a statement about what it means to be a being endowed with reason, as humans are.  To have reason is to be a self, and 'settings one's own being originally absolutely' means that the self ultimately takes responsibility for anything it does.  Any activities the self undertakes, the self necessarily subscribes to itself as its own activities that it has carried out. In ascribing these actions to its self, it thereby  takes responsibility for them.  Such a paraphrase might seem distant from the wording that Fichte has chosen, but we'll get into that later. 
+
+## The second principle
+The second principle introduces the concept of the Non-self.
+~~~
+'A Non-self is absolutely set against the self.
+
+...dem Ich [wird] schlechthin entgegengesetzt ein Nicht-Ich.
+~~~
+The idea here is simple: there exists for the self a non-self.  The 'non-self' is quite literally anything that the self is not.  This non-self therefore referes to the world at large.  Whereas my thoughts, actions, and perceptions belong to me, the chair or the table in front of me does not, and is therefore part of the non-self.  Making a sharp distinction between where the self ends and the non-self begins is a tricky business. For example, is one's body part of the self or the non-self?  In the _Grundlage des Naturrechts_ (s.57) Fichte will say that the body belongs to the non-self, since it is not a part of the self's 'selfhood' (_Ichheit_), but it has a specific status vis-a-vis the self as part of the non-self that only the self has jurisdiction over.  We can in general say that the 'Self' is everyting that has to do with the self's exercise of reason, and the non-self is everything else.  John McDowell's distinction between _mind_ and _world_ is helpful here.
+
+
+## The third principle
+
+The third principle can be thought of as the motor of the _Wissenschaftslehre_.  It sets off a chain of thought that together comprises the rest of the system.  It does this by point out the contradiction between the Self and the Non-Self and then implying a common ground between the Self and Non-Self that would reconcile this contraction.
+~~~
+I set in the Self a divisible Non-Self over and against a divisible Self.   
+ Ich setze im Ich dem theilbaren Ich ein theilbares Nicht-Ich entgegen.
+ ~~~
+
+A lot is packed into the notion of 'divisibility' here.  We can identify two main aspects of the term.
+1. When something is posited as 'divisible' it is posited not in its entirely, but rather partially, or, as Fichte says, in terms of a certain 'quantity'.  When something is posited to a certain extent, or as a quantity, Fichte says that it is 'limited.'  To posit something partially is to both posit it and limit it, to posit a certain quantity of that thing.  It follows from the fact that the Self and the Not-Self are the only two entities in play that they each limit eachother.  Fichte will elaborate in the _Theortical Part_ on the precise nature of this mutual limitation, but for starters, the reciprocal limitation of the Self and the Non-Self allows Fichte to split the remainder of the _Wissenschaftslehre_ into the _Theoretical_ and the _Practical_.  This is shown in the following diagram:
 @@SVG1@@
-The Theoretical and Practical Parts of Doctrine of Knowledge then have the task of rendering explicit everything that these three claims together entail./nWe can begin to see how that works looking ahead to what Fichte says at the beginning ofthe Theortical Part.  There are two ways in which the I partially posits both itself and a Not-I.  The first is that the I posits itself as determiend by the Not-I [Get the exact quote], and forms the basis of his theoretical philosophy. The second is.   This layout can be expressed in the following diagram:
+In so far as the Self is limiting the Non-Self, the Self is acting in its capacity as a practical being.  It is acting on the world.  In so far as the Non-Self is limiting the Self, the Self is perceiving the world, it is conforming its perceptions to reality that the world presents it with.
+2. When we posit something partially, we assume that it shares something in common with whatever limits it. This Fichte calls this the 'ground of relation' (Beziehungsgrund).  The thing, X, and the thing that limits X, ¬X, must therefore share a common ground that allows them to be set in the first place into relation to one another.  This raises a question that Fichte sets out to answer in the remaineder of the _Wissenscahftslehre_, namely: what is the ground of relation between the Non-Self and the Self. What is it that they share in common, that allows them come in contact with one another in the first place and thereby limit each other?
+
+## The remainder of the _Wissenschaftslehre_
+The uniqueness of Fichte's entire philosophy turns on this need to identify the common ground between the Self and the Non-Self. It is what makes Fichte's philosophy monist and not dualist.  Kant, for example, delivers a dualist system, consisting of a fundamental difference between the transcendental Self and the thing-in-itself.  Spinoza, a monist, argues that the thoughts and actions of people are modifications of one all-encompassing substance.  In the terms of Fichte's system, he finds the common ground of the Self and the Non-Self to reside in the Non-Self, of which the Self is a small part or modification.
+
+Fichte's monism claims that the Self forms the common ground between the Self and the Non-Self.  Fichte has already tipped his hand in the _Principles of the entire Wissenschaftslehre_ that this is going to be his answer to the question posed on in the Third Principle.  For the absoluteless and originality of the Self is his starting principle, and the third principle indicates that the partial positing of the Self and the Not-Self resides in the Self ('I set in the Self...') However, the full monist picture is only going to emerge at the conclusion of the _Theoretical Part_.
+
+At the conclusion of the _Theoretical Part_ Fichte describes a kind of journey, where in the beginning there is only the Self.  After an initial impulse (Anstoss) that limits the all-encompassing Self, the Non-Self then appears for the Self.  It is only at this derivative point that the situation looks dualist.  However, in the _Practical Part_ Fichte describes the rest of the journey, whereby the Self's purpose in life is to act in a way so as to recover this initial all-encompassing Self by acting as a moral being.  Thus, the Non-Self is only a intermediate station in the Self's journey of Self-realization. Fichte's system completes itself by ending with the principle from which it started: the absolute, originally 'setting' or 'positing' of the Self.  This journey is described in the following diagram, where the arrow represents the path that the Self follows.  Note that the journey is never completed in reality, hence the dotted lines in the digram.  The attempt to realize the Self is an infinite 'striving' that is never actually fulfilled:
+@@SVG2@@
+
+In the following diagram, we overlay the various principles of the _Groundwork_ on top of the journey, so that the reader can see how the principles together render this final picture of Fichte's philosophy:
+@@SVG3@@
+
+## What does it mean to posit, anyway?
+
+## What does it mean to say that the Self 'posits' itself?
+
+## This sounds kind of egotistical, to say that there is only the Self.  What's going on?
 
 """
