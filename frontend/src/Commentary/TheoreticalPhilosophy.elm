adceb8271e1a183c32cec35515d896c5cf9d3dff
Marcus <marcus.lampert@pwc.com>
2019-09-10 09:21:22 +0200

    Finish general concepts of exchange and independent activity

--- frontend/src/Commentary/TheoreticalPhilosophy.elm
+++ frontend/src/Commentary/TheoreticalPhilosophy.elm
@@ -16,40 +16,51 @@ Fichte's goal is this section is to figure out how the _Theoretical Principle_ i
 2.  In the second stage, Fichte uses these six concepts to map out various accounts of the _Theoretical Principle_.  These accounts are all versions of _realism_ or _idealism_.
 3.  At the conclusion of the second stage, Fichte puts forth a new account that is entirely different in character from the _idealist_ nor _realist_ accounts he has thus far set up. Whereas the previous accounts are all dualist in that they assume a Self and a Not-Self as their starting point, this account is monist, since it takes the Self to be the starting point, and then describes how, a Not-Self emerges from the Self.  This is Fichte's so-caled 'theory of the _check_' (Anstosslehre), and the remainder of the _Theoretical Part_ is devoted to fleshing out this account.
 
-Fichte introduces and employs his six guiding concept in a highly structured manner that we can map out as follows:
+(Note that this three stage division is an artificial construct that is not delineated in the actual text, but that we have introduced for explanatory purposes.)
+
+The guiding thread throughout all three stages of the _Theoretical Part_ are six guiding concepts that Fichte employs in a highly structured manner that we can map out as follows:
 @@SVG3@@
-This diagram is to be read from top to bottom.  Fichte begins by introducing two general concepts, causality and substance, that each have a different way of specifying how the _Theoretical Principle_ works.
+We can overlay the three stages idenitfied above on this roadmap as follows:
+@@SVG3@@
+
+This diagram is to be read from top to bottom, and then, approximately, from left to right.  In stage 1, Fichte is introducing his six concepts and explaining how these concepts combine so as to isolate eight different aspects of the _Theoretical Principle_.  In stage two, Fichte is showing how these six concepts, or eight aspects work together to form incomplete accounts of the _Theoretical Principle_.  In stage 3, we then get the complete, or the monist theory of the check.
+
+Let's look at each stage in more detail.
+
+### Stage 1
+
+Fichte begins by introducing two general concepts, causality and substance, that each have a different way of specifying how the _Theoretical Principle_ works.
 * _Causality_ says that the Self and the Non-Self stand in a relationship of efficient causality to one another.  Since all positing, according to Fichte's _First Principle_ (or also the _Third Principle_), starts from the Self, the Non-Self only has reality for the Self insofar as the Self posits it.  The question is thus the following: what is causing, or _affecting_, the Self to posit the Non-Self, to endow it with reality, or, as Fichte also formulates it, trasfer activity to the Non-Self? The concept of causality leaves this question open, but limits the answer to two possibilities:
     1. The Self is intiating the action of the _Theoretical Principle_, affecting itself such that it posits the Non-Self.
     2. The Non-Self is initiating the of the _Theoeretical Principle_, affecting the Self, and therefore causing the Self to undergo its positing of the Non-Self.
 * _Substance_ says that Self is a substance and the Non-Self is an accident of the Self (not an _accident_ in the sense of a _mistake_, but rather in the formal philosophical sense that green is an accident of the substance tree, for example).  The notion of the Self as a substance follows more or less from the _First_ and _Third Principles_, which says that all positing is done by the Self.  Therefore Fichte can say that the Self contains the set of 'all realities'. The Non-Self is then a subset of that complete set of all realities.
 
 This explains the first division at the top of the diagram.  The next division comes because Fichte wants to develop a more detailed picture of what a causal and a substantial interaction between Self and Non-Self looks like.  In order to do this, he introduces another pair of concepts, _exchange_ and _independent activity_, both of which can be applied to the cases of causality and substance.
-* The _exchange of activity and passivity_, also simply called an _exchange_, describes a kind of tug-of-war in which the Self and the Non-Self are engaged.  Any amount of activity in the one, corresponds to the same amount of passivity in the other. So, the Non-Self is active to the exact degree that the Self is passive, and visa versa.
-* Indendent activity is an activity that allows the Self to be aware of the tug-of-war with the Non-Self in which it is engaged.  The first concept, _exchange_ would be enough to explain that the Non-Self determines the Self.  Namely, the Non-Self acts on the passive Self.  However, the _Theoretical Principle_ also says that the Self posits itself _as_ determined by the Non-Self.  It is thus important not only that the Non-Self determine the Self, but also that the Self be aware of this determine.
+* The _exchange of activity and passivity_, also simply called an _exchange_, describes a situation in which one entity is determining another.  Any amount of activity in the one enitity, corresponds to the exact same amount of passivity, or suffering, in the other. The active entity is determining the passive entity, which is absorbing, or taking in the activity of the active entity.  It is called an _exchange_ or _interaction_, because the activity of the one entity fully determines the passivity of the other, and visa versa.  Thus is specificies a formula for _exchanging_ the activity or passivity observed in the one entity with activity or passivity that must be occuring in the other.  The specific exchange that interests Fichte in the _Theoretical Part_ is the activity of the Non-Self and its correspondence to the passivity of the Self.   
+* Whereas an exchange of activity and passivity applies to any kind of object, and most notably physical objects, indendent activity is an activity that allows the Self to be aware of the exchange with the Non-Self in which it is engaged.  The first concept, _exchange_ would be enough to explain that the Non-Self determines  Self, for it says that the Non-Self acts on the passive Self.  However, the _Theoretical Principle_ also says that the Self posits itself _as_ determined by the Non-Self.  It is thus important not only that the Non-Self determine the Self, but also that the Self be aware of this determination.  Fichte calls this awareness _independent activity_ because he considers being aware to be an _act_. This _act_ or _activity_ is _independent_ in that it is not caught up in the direct _exchange_ between the Non-Self and the Self.  The passivity of the Self, when it is being determined by the Non-Self, does not affect the activity that goes into being aware of its passivity.   
 
-In later sections, we will look in more detail at the application of these concepts to causality and substance.  For now we can gloss them as follows:
- 
+Fichte now applies these two concepts to the two concepts of causality and substance so as to now describe four different aspects at play in the _Theoretical Principle_.
 
-the transfer of some quantity of activity from one entity to another.  In the case  
+* Causal exchange: 
 
+* Causal independent activity:
 
-affects theThe Self is positing itself as determined Either
-    1) the Non-Self is initiating the action, meaning it affects the Self, thereby cause the Self to posit itself as determiend by the Non-Self. 
+* Substantial exchange:
 
+* Substantial indendendent activity:
 
-In order to explain the founding principle, Fichte employs six main concepts :
-*Substance
-*Causality
-*Independent Activiy
-*Exchange
-*Form
-*Matter
+The last step involves introducing the formal and material aspects of these four aspects.
+Fichte defines form and matter in a very abstract manner.
 
-Each of these concepts isolate some aspect of mind-world interaction.  They are most useful in the investigation when used in combination with one another.  The six concepts divide into three pairs, each of which belongs to what we can call its own tier.  Form and matter belong to the tier of sensuousness. They are concerned with the sensuous nature of mind-world interaciton, the fact that the mind perceives the world as consisting of matter that takes on various forms.  The second tier we can call the dynamic tier. The third we can call the categorical tier.
+Matter and form are guided by the notions of sensuous matter and form.  It takes some effort to see this.
 
 
-## The tendency of _The Theoretical Part_
+### Stage 2
+  What does it mean to have one concept 'determine' the other?  What does it mean to have form determine matter, and visa versa? The matter explains the form.  The form explains the matter.
+
+
+### Stage 3
+ The tendency of _The Theoretical Part_.  
 
 
 
