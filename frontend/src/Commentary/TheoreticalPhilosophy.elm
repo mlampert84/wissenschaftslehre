f4e412ab685a3895e17b3956798d0d580b83f3f5
Marcus <marcus.lampert@pwc.com>
2019-09-21 13:20:17 +0200

    Redo beginning of theoretical philosophy

--- frontend/src/Commentary/TheoreticalPhilosophy.elm
+++ frontend/src/Commentary/TheoreticalPhilosophy.elm
@@ -4,26 +4,65 @@ module Commentary.TheoreticalPhilosophy exposing (content)
 content =
     """# Theoretical Philosophy
 
-The _Theoretical Part_ of the _Wissenschaftslehre_ is concerned with how we know things about the world.  Fichte's describes it as an investigation into resolving the contradictions contained in the founding principle of theoretical philosophy, which we can simply refer to as the _Theoretical Principle_:
+The _Theoretical Part_ of the _Wissenschaftslehre_ is concerned with how we know things about the world.  It investigates this question by explaining how the founding principle of theoretical philosophy, which we can simply refer to as the _Theoretical Principle_ is possible.  The _Theoretical Principle_ says the following:
 ~~~The self sets itself as determined by the Non-Self.
 Das Ich setzt sich als bestimmt durch das Nicht-Ich.~~~
 We can parse this principle as follows: the knowledge that we have about the world is our knowledge, since we are the ones doing the knowing.  Hence the formulation that 'the self sets itself'.  However, that which we have knowledge of is not of our making, but rather concerns the world. We must comply with what the world presents to us, otherwise it's our invention and not knowledge.  Hence the self is 'determined by the Non-Self.'  Since we are in the theoretical realm, 'mind' is a good stand-in term for the Self, so we can say that we are dealing with the interaction between mind and world, where the mind, in so far as it is focused on the world, takes itself to be determined by the world.
 
 
-## The outline of _The Theoretical Part_
-Fichte's goal is this section is to figure out how the _Theoretical Principle_ is possible.  We can roughly divide the phases of Fichte's investigation into three stages:
-1.  In a first stage, Fichte introduces a series of six concepts that he believes are essential to explain the _Theoretical Principle_.
-2.  In the second stage, Fichte uses these six concepts to map out various accounts of the _Theoretical Principle_.  These accounts are all versions of _realism_ or _idealism_.
-3.  At the conclusion of the second stage, Fichte puts forth a new account that is entirely different in character from the _idealist_ nor _realist_ accounts he has thus far set up. Whereas the previous accounts are all dualist in that they assume a Self and a Not-Self as their starting point, this account is monist, since it takes the Self to be the starting point, and then describes how, a Not-Self emerges from the Self.  This is Fichte's so-caled 'theory of the _check_' (Anstosslehre), and the remainder of the _Theoretical Part_ is devoted to fleshing out this account.
+## The different stages of _The Theoretical Part_
+Fichte's goal is this section is to figure out how the _Theoretical Principle_ is possible, and that means resolving its central contradiction.  On the one hand, the Non-Self is determining the Self.  On the other, the Self is _taking itself_ to be determined, and this act of _taking onself_ to be something is also kind of determination.  Thus, determination flows in two contradictory directions.  The Non-Self _determines_ the Self.  However it is also true that the Self is _determining_ this determination of the Self by the Non-Self. So which of the two entities is ultimately determining the other?
 
-(Note that this three stage division is an artificial construct that is not delineated in the actual text, but that we have introduced for explanatory purposes.)
+Fichte takes this contradiction to be his formulation of the contradiction between realism and idealism.  A realist would say that the Non-Self is ultimately the cause of the Self taking itself to be determined by the Non-Self.  An idealist would argue that in fact the Self is determining or causing the determination, or limitation, that the Non-Self appears to be imposing on the Self.  In this latter case, the _Theoretical Principle_ is a case of Self-determination, in the former, the Self is being determined by something outside itself.
 
-The guiding thread throughout all three stages of the _Theoretical Part_ are six guiding concepts that Fichte employs in a highly structured manner that we can map out as follows:
+Fichte aims to resolve this contradiction by identifying its two competing sides with two complementary pairs of philosophical terms that normally appear together.  Once such pair of terms that Fichte identifies is form and matter.  Fichte in fact introduces two additional complementary pairs of terms as well, but for now we can work with a simplified model so as to more generally describe Fichte's procedure.  With the help of these two complementary terms, Fichte can then resolve the contradiction by showing that the two sides of the _Theoretical Principle_ are in fact not in contradiction with one another, but rather mutually constitute one another in a relationship of complementarity.
+
+Fichte divides this overall procedure into two stages, an analytic and a synthetic stage. In the analytic stgae, he describes the contradiciton of the _Theoretical Principle_ in terms of a tension beween his two philosophical terms, in our case form and matter.  In the syntehtic phase he argues for the complementarity of the two sides of the contradiction, thereby transforming the apparent contradiction into a unified account of the _Theoretical Principle_.
+
+
+Let us now walk through this procedure with form and matter.  In the analytical stage, we identify a formal aspect and a material aspect at work in the contradictory acts of the Self and the Non-Self each trying to determine the other.  We can accomplish this by observing that the world consists of sensous matter that is arranged in various forms.  If we express this distinction in terms of perception, then we can say that to percieve the world is to percieve a senuous mannifold (matter) that we then synthesize into unified forms. So, for example, in perceiving a table, all of the senous matter that belongs to the table would be unified according to our perception in the form of the table.  Such a way of looking at things allows us to attribute the sensous matter to the Non-Self, and the unification of the matter into a specific form, to the determining act of the perceiving Self.  Thus, we can say that the Self is the determining agent in the _Theoretical Principle_ in so far as it provides the sensuous forms that we perceive in the act of cognition, and the Non-Self is the determining agent in the _Theoretical Principle_ in so far as it provides the sensuous matter.  The formal determination of our world is a act of the Self, and the material determination of our world is an act of the Non-Self.
+
+It shoudl be noted that such a division of form to the Self and matter to the Non-Self follows Kant's account in the _Critique of Pure Reason_ (and even more obviously Rheinhold's account in his _Elementary Philosophy_).  According to Kant, the manifold of sensibility is supplied to the conscious subject through affection by the Thing-in-Itself, and the subject then unifies this manifold by applying a concept to the manifold.  When this application of a concept occurs, we percieve a unified sensuous form that is a product of our determination, filled with the matter of sensisibility that the Thing-in-itself supplied.  Fichte is thus working along Kantian lines, whereby the matter belongs to the Non-Self, and the form to the Self.
+
+In the next stage, the synthesis, the question is which of the two aspects, i.e. matter or form, determines the other.  At stake here is the question of which entity, the Self or the Non-Self, is ultimately the determining agent in the _Theoretical Principle_, and thus whether we are dealing with a case of idealism or realism.  Of course, Fichte wants to claim that neither of the two is the determining aspect, and that these aspects in fact mutually determine one another.  Before he does that, however, he considers each side of the contradiction in order to then more fully articulate the middle path that takes to be the correct account.
+
+In the idealist side of the dilemma, we consider what it means to say that form determines matter. 
+
+The analysis occurs by distinguishing between form and matter. Such a distinction 
+allows us to tease apart two aspects that are in tension with one another in the _Theoretical Principle_.  
+The distinction between form and matter allows us to 1) articuate this contradiction more fully and then 2)provide a resolution to this contradiction.  The first step is the analysis, the second is the synthesis.  
+
+
+
+We can roughly divide the phases of Fichte's investigation into three stages:
+1.  The analysis stage.  In this stage, Fichte introduces a series of six concepts that he believes are essential to explain the _Theoretical Principle_.  He uses these six concepts to distinguish eights aspects at work in this principle. 
+2.  The synthesis stage.  In this stage, Fichte synthesizes the eight aspects of the _Theoretical Principle_.  A synthesis consists is showing how two aspects, one which is realist, and another which is idealist, mutually determine each other to arrive at an account that is neither realist nor idealist.
+3.  At the conclusion of the second stage, Fichte puts forth a new account that is markedly different in character from the _idealist_ and _realist_ accounts he has thus far set up. Whereas the previous accounts are all dualist in that they assume a Self and a Not-Self as their starting point, this account is monist, since it takes the Self to be the starting point, and then describes how a Not-Self emerges from the Self.  This is Fichte's so-called 'theory of the _check_' (Anstosslehre), and the remainder of the _Theoretical Part_ is devoted to fleshing out this account.
+
+The guiding thread throughout all three stages of the _Theoretical Part_ are the six guiding concepts that Fichte employs in a highly structured manner that we can map out as follows:
 @@SVG3@@
 We can overlay the three stages idenitfied above on this roadmap as follows:
 @@SVG3@@
 
-This diagram is to be read from top to bottom, and then, approximately, from left to right.  In stage 1, Fichte is introducing his six concepts and explaining how these concepts combine so as to isolate eight different aspects of the _Theoretical Principle_.  In stage two, Fichte is showing how these six concepts, or eight aspects work together to form incomplete accounts of the _Theoretical Principle_.  In stage 3, we then get the complete, or the monist theory of the check.
+We are going to have to take several passes at explaining this diagram, for it is very abstract and requires a lot of explanation before its meaning becomes clearer.  The diagram is to be read from top to bottom, and then, approximately, from left to right.
+
+In the analysis stage, Fichte is introducing his six concepts. First he introduces the concepts of causality and substance - as shown at the top of the diagram. Next, he introduces the concepts of an _exchange of activity and passivity_, or _exchange_ for short, and then the concept of independent activity. These two concepts are then applied as special cases to the concepts of causality and substance, so that we have four different possibilities for explaining the _Theoretical Principle_, namely causal exchange, causal idenpendent activity, substantial exchange, and substantial independent activity.  In the last step of stage 1, Fichte introduces the concepts of form and matter, and applies these as further specifications to the four-fold division of the last step.  Thus, at the end of stage 1, we have the material aspect of causal exchange and the formal aspect of causal exchange. Then we have the material aspect of independent activity, and so on.
+
+In the synthesis stage, Fichte shows how the various aspects he has identified mutually determine one another.  There are a total of six synthesis: material exhange causation synthesized with formal exchange causation, material independent activity in causation with material independent activity in causation, etc.  Each of the six syntheses in the digram follows an the same procedure.  First, Fichte considers what it means to say that one aspect determines the other.  Then, Fichte completes the synthesis by showing how the two aspects mutually determine each other.
+
+The final, sixth synthesis, is the point at which Fichte arrives at the explaination of the _Theoretical Principle_ that he considers to be an accurate explanation.
+
+## The outline of the _Theoretical Part_ in slightly more detail
+
+Let us now look at the analytic and synthetic phases in more detail.  For convienience of explanation, we can start in the middle of the digramn with the concepts of form and matter, since these are concepts that are least bound to the idiosyncracies of Fichte's system.  Let us more away from the diagram and consider form and matter just by themselves, unconnected to the other two levels of distinction. 
+
+
+
+pitting the formal aspects that have been identified against material aspects, and then the _exchange_ aspects against _idenpendent activity_ aspects.  The  
+It is essential for the second stage What aspects of the _Theoretical Principle_ do these six terms represent?
+
+
+ and explaining how these concepts combine so as to isolate eight different aspects of the _Theoretical Principle_.  In stage two, Fichte is showing how these six concepts, or eight aspects work together to form incomplete accounts of the _Theoretical Principle_.  In stage 3, we then get the complete, or the monist theory of the check.
 
 Let's look at each stage in more detail.
 
@@ -31,9 +70,9 @@ Let's look at each stage in more detail.
 
 Fichte begins by introducing two general concepts, causality and substance, that each have a different way of specifying how the _Theoretical Principle_ works.
 * _Causality_ says that the Self and the Non-Self stand in a relationship of efficient causality to one another.  Since all positing, according to Fichte's _First Principle_ (or also the _Third Principle_), starts from the Self, the Non-Self only has reality for the Self insofar as the Self posits it.  The question is thus the following: what is causing, or _affecting_, the Self to posit the Non-Self, to endow it with reality, or, as Fichte also formulates it, trasfer activity to the Non-Self? The concept of causality leaves this question open, but limits the answer to two possibilities:
-    1. The Self is intiating the action of the _Theoretical Principle_, affecting itself such that it posits the Non-Self.
-    2. The Non-Self is initiating the of the _Theoeretical Principle_, affecting the Self, and therefore causing the Self to undergo its positing of the Non-Self.
-* _Substance_ says that Self is a substance and the Non-Self is an accident of the Self (not an _accident_ in the sense of a _mistake_, but rather in the formal philosophical sense that green is an accident of the substance tree, for example).  The notion of the Self as a substance follows more or less from the _First_ and _Third Principles_, which says that all positing is done by the Self.  Therefore Fichte can say that the Self contains the set of 'all realities'. The Non-Self is then a subset of that complete set of all realities.
+    1. The Self is initiating the action of the _Theoretical Principle_, affecting itself such that it posits the Non-Self.
+    2. The Non-Self is initiating the action of the _Theoeretical Principle_, affecting the Self, and therefore causing the Self to undergo its positing of the Non-Self.
+* _Substance_ says that Self is a substance and the Non-Self is an accident of the Self (not an _accident_ in the sense of a _mistake_, but rather in the formal philosophical sense that green is an accident of the substance tree, for example).  The notion of the Self as a substance follows more or less from the _First_ and _Third Principles_, which say that all positing is done by the Self.  Therefore Fichte can say that the Self contains the set of 'all realities'. The Non-Self is then a subset of that complete set of all realities.
 
 This explains the first division at the top of the diagram.  The next division comes because Fichte wants to develop a more detailed picture of what a causal and a substantial interaction between Self and Non-Self looks like.  In order to do this, he introduces another pair of concepts, _exchange_ and _independent activity_, both of which can be applied to the cases of causality and substance.
 * The _exchange of activity and passivity_, also simply called an _exchange_, describes a situation in which one entity is determining another.  Any amount of activity in the one enitity, corresponds to the exact same amount of passivity, or suffering, in the other. The active entity is determining the passive entity, which is absorbing, or taking in the activity of the active entity.  It is called an _exchange_ or _interaction_, because the activity of the one entity fully determines the passivity of the other, and visa versa.  Thus is specificies a formula for _exchanging_ the activity or passivity observed in the one entity with activity or passivity that must be occuring in the other.  The specific exchange that interests Fichte in the _Theoretical Part_ is the activity of the Non-Self and its correspondence to the passivity of the Self.   
